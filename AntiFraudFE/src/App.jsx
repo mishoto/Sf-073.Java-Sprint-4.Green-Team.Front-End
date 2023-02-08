8eb03e813d426554443da6950a338abd19ca5fda
mishoto <mihail.dev.io@gmail.com>
2023-02-08 23:13:35 +0200

    initial commit

---
+++ AntiFraudFE/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <h3>Name: AntiFraudFE</h3>
+    <h4>Framework: react</h4>
+    <h4>Language: JavaScript</h4>
+    <h4>CSS: Empty CSS</h4>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ AntiFraudFE/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    publicPath: "http://localhost:3040/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3040,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+      new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
