808ae8a676c2e31ac54fbfa9f270ba1ec7734945
mishoto <mihail.dev.io@gmail.com>
2023-02-14 12:02:50 +0200

    edit class -> classNames and remove <body> from DOM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,28 +7,27 @@ import img4 from "../images/georgivase.jpg";
 const Home = () => {
   return (
     <div>
-      <body>
-        <h1>Anti-Fraud System By:</h1>
-        <div class="team-presentation">
-          <div class="member">
+      <h1>Anti-Fraud System By:</h1>
+        <div className="team-presentation">
+          <div className="member">
             <img src={img1} alt="Member 1" />
             <h3>Georgi Tangardzhiev</h3>
             <p>Position: Trainee</p>
             <p>Skills: Java, Spring, HTML&CSS</p>
           </div>
-          <div class="member">
+          <div className="member">
             <img src={img2} alt="Member 2" />
             <h3>Mihail Mihailov</h3>
             <p>Position: Trainee</p>
             <p>Skills: Java, Spring, JavaScript</p>
           </div>
-          <div class="member">
+          <div className="member">
             <img src={img3} alt="Member 3" />
             <h3>Silviya Yankova</h3>
             <p>Position: Trainee</p>
             <p>Skills: Java, Spring, JavaScript</p>
           </div>
-          <div class="member">
+          <div className="member">
             <img src={img4} alt="Member 4" />
             <h3>
               Georgi <br />
@@ -38,7 +37,6 @@ const Home = () => {
             <p>Skills: Java, Spring</p>
           </div>
         </div>
-      </body>
     </div>
   );
 };
